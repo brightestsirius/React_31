9655a37f7a4355d6428a63093e6567946dbca773
brightestsirius <katherina.shypovskaya@gmail.com>
2025-12-19 21:20:58 +0200

    lesson_00

--- lesson_00/script.js
+++ lesson_00/script.js
@@ -1,58 +1,90 @@
-const domContainer = document.querySelector("#root");
-const root = ReactDOM.createRoot(domContainer);
+const rootContainer = document.querySelector(`#root`);
+const rootContainerReact = ReactDOM.createRoot(rootContainer);
 
-// const heading_1 = React.createElement('h1', {className: `heading_1`}, `Hello, world!`);
-// const heading_2 = React.createElement('h2', {className: `heading_2`}, `Description`);
-// const container = React.createElement('div', null, heading_1, heading_2);
+const heading_1 = React.createElement(
+  `h1`,
+  { id: `heading`, className: `heading` },
+  `Hello, world`
+);
 
-const heading_1 = `Hello, world!`;
-const animals = [`cat`, `dog`, `lion`];
+const heading_1_jsx = (
+  <h1 id="heading" className="heading">
+    Hello, world!!!
+  </h1>
+);
+const heading_2 = (
+  <h2 id="heading" className="heading">
+    Description
+  </h2>
+);
+
+const animals = [`cat`, `dog`, `lion`, `lion`];
+
 const users = [
-  {
-    id: 1,
-    name: `User 1`,
-    age: 10,
-  },
-  {
-    id: 2,
-    name: `User 2`,
-    age: 20,
-  },
-  {
-    id: 3,
-    name: `User 3`,
-    age: 30,
-  },
-];
+    {
+        id: 1,
+        name: `User_1`,
+        age: 10
+    },
+    {
+        id: 2,
+        name: `User_2`,
+        age: 20
+    },
+    {
+        id: 3,
+        name: `User_3`,
+        age: 30
+    }
+]
 
-const List = ({ list }) => {
-  return Array.isArray(list) ? (
-    <ul>
-      {list.map(({ id, ...rest }) => (
-        <li key={id}>
-          <ul>
-            {Object.entries(rest).map(([key, value]) => (
-              <li key={key}>
-                {key}: {value}
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
-  ) : null;
-};
-
-const App = () => {
-  return (
+rootContainerReact.render(
+  <React.Fragment>
     <React.Fragment>
-      <h1>{heading_1}</h1>
-      <h2>Description</h2>
-
-      <List list={users} />
-      <List />
+      {heading_1_jsx}
+      {heading_2}
     </React.Fragment>
-  );
-};
+    <hr></hr>
+    <React.Fragment>
+      {heading_1_jsx}
+      {heading_2}
+    </React.Fragment>
 
-root.render(<App />);
+    <ul>
+        {animals.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+
+    <ul>
+        {users.map(item => <li key={item.id}>Name: {item.name}, Age: {item.age}</li>)}
+    </ul>
+
+    <ul>
+        {
+            users.map(item => <li key={item.id}>
+                <ul>
+                    {Object.entries(item).map(([key, value]) => <li key={key}>{key}: {value}</li>)}
+                </ul>
+            </li>)
+        }
+    </ul>
+
+    {10>5 ? <h1>Hello</h1> : <h2>No</h2>}
+  </React.Fragment>
+);
+
+{/* <ul>
+    <li>
+        <ul>
+            <li>id: 1</li>
+            <li>name: User_1</li>
+            <li>age: 10</li>
+        </ul>
+    </li>
+    <li>
+        <ul>
+            <li>id: 1</li>
+            <li>name: User_1</li>
+            <li>age: 10</li>
+        </ul>
+    </li>
+</ul> */}
--- lesson_01/src/components/Counter/CounterResult.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CounterResult({ counter }) {
-  return <span>Counter Result: {counter}</span>;
-}
