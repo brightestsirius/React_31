c960b708493eff4d7f81bdfebdcbb3f996bac544
brightestsirius <katherina.shypovskaya@gmail.com>
2025-12-23 17:19:08 +0200

    lesson_02 - intro

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,4 +1,40 @@
 const rootContainer = document.querySelector(`#root`);
 const rootContainerReact = ReactDOM.createRoot(rootContainer);
 
-rootContainerReact.render();
+const users = [
+  {
+    id: 1,
+    name: `User_1`,
+    age: 10,
+  },
+  {
+    id: 2,
+    name: `User_2`,
+    age: 20,
+  },
+  {
+    id: 3,
+    name: `User_3`,
+    age: 30,
+  },
+];
+
+rootContainerReact.render(
+  <React.Fragment>
+    <ul>
+      {users.map((item) => (
+        <li key={item.id}>
+          <ul>
+            {Object.keys(item)
+              .filter((item) => item !== `id`)
+              .map((key) => (
+                <li key={key}>
+                  {key}: {item[key]}
+                </li>
+              ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  </React.Fragment>
+);
